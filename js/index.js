729bd1825e35e15ed2679c727ca0d20ddab47d6d
Martijn Kooijman <martijnkooijman12@gmail.com>
2024-03-27 10:32:26 +0100

    Setback

--- js/index.js
+++ js/index.js
@@ -21,39 +21,34 @@ fetch(apiUrl)
         mc2023 = data["2023"].mc;
         attendees2023 = data["2023"].attendees;
 
-    const speakersContainer = document.getElementById("speakers-container");
-    let counter = 0;
+        const speakersContainer = document.getElementById("speakers-container");
+        let counter = 0;
 
-    function createCard(container, name, title, link, description, avatar, day, country, attendees) {
-      counter++;
+        function createCard(container, name, title, link, description, avatar, day, country, attendees) {
+            counter++;
 
+            // Maak een element aan voor de kaart
+            const card = document.createElement("a");
+            card.id = `card-${counter}`;
+            card.href = `#card-${counter}`;
 
-      // Maak een element aan voor de kaart
-      const card = document.createElement("article");
-      card.id = `card-${counter}`;
-      
-      card.classList.add("card");
+            card.classList.add("card");
 
-      // Voeg inhoud toe aan de kaart
-      card.innerHTML = `
-      <label>
-          <input type="radio" name="sprekers" value="${name}">${name}</input>
-      </label>
-      <label>
-          <input type="radio" name="sprekers" value="sluiten"></input>
-      </label>
-      <ul>
-        <li><img src="${avatar}" alt="image of ${name}"></li>
-        <li><h2>Name: ${name}</h2></li>
-        <li>Nationality: ${country}</li>
-        <li>Title: ${title}</li>
-        <li>Description: ${description}</li>
-        <li>Speaks on: ${day}</li>
-        <li>Website: ${link}</li>
-        <li><input type="radio" name="card-btn" id="card-btn-${counter}"></li>
-      </ul>
-
-      `;
+            // Voeg inhoud toe aan de kaart
+            card.innerHTML = `
+              <article>
+                <a href="#sluiten" class="sluiten"></a>
+                <ul>
+                  <li><img src="${avatar}" alt="image of ${name}"></li>
+                  <li><h2>Name: ${name}</h2></li>
+                  <li>Nationality: ${country}</li>
+                  <li>Title: ${title}</li>
+                  <li>Description: ${description}</li>
+                  <li>Speaks on: ${day}</li>
+                  <li>Website: ${link}</li>
+                </ul>
+              </article>
+            `;
 
             // Voeg de kaart toe aan de container
             container.appendChild(card);
@@ -64,24 +59,23 @@ fetch(apiUrl)
             const speakerInfo = speakers2023[speakerKey];
             let speakerInfoDesc;
 
-      if(speakerInfo.talk.description === false) {
-        speakerInfoDesc = "No description yet :)";
-      } else {
-        speakerInfoDesc = speakerInfo.talk.description;
-      }
+            if (speakerInfo.talk.description === false) {
+                speakerInfoDesc = "No description yet :)";
+            } else {
+                speakerInfoDesc = speakerInfo.talk.description;
+            }
 
-      createCard(speakersContainer, speakerInfo.name, speakerInfo.talk.title, 
-        speakerInfo.link, speakerInfoDesc, speakerInfo.avatar, speakerInfo.day, speakerInfo.country, speakerInfo.attendees);
+            createCard(speakersContainer, speakerInfo.name, speakerInfo.talk.title,
+                speakerInfo.link, speakerInfoDesc, speakerInfo.avatar, speakerInfo.day, speakerInfo.country, speakerInfo.attendees);
+        });
+
+        // Itereer over de mc's en maak een kaart voor elke mc
+        Object.keys(mc2023).forEach(mcKey => {
+            const mcInfo = mc2023[mcKey];
+            createCard(speakersContainer, mcInfo.name, "", mcInfo.link, "", mcInfo.avatar, "", "", "");
+        });
+    })
+    .catch(error => {
+        // Vang eventuele fouten op
+        console.error('There was a problem with the fetch operation:', error);
     });
-
-    // Itereer over de mc's en maak een kaart voor elke mc
-    Object.keys(mc2023).forEach(mcKey => {
-      const mcInfo = mc2023[mcKey];
-      createCard(speakersContainer, mcInfo.name, "", mcInfo.link, "", mcInfo.avatar, "", "", "");
-    });
-  })
-  .catch(error => {
-    // Vang eventuele fouten op
-    console.error('There was a problem with the fetch operation:', error);
-  });
-
