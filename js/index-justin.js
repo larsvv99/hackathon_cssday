f3f38ff3005bda3a1ce980299f209e0d4561c8c1
JustinPrins98 <justinprinsroc1998@gmail.com>
2024-03-27 13:31:17 +0100

    Flip card

---
+++ js/index-justin.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+
+const card = document.getElementById("card")
+
+card.addEventListener("click", flipCard);
+
+function flipCard() {
+    card.classList.toggle("flipCard");
+}
